b31ea17c1a8fba95203f5a4d9260352e2a205a41
hasanaimroatun <imroatunhasana@gmail.com>
2022-12-09 10:25:44 +0700

    create file, add elements and import css

---
+++ src/components/Jobs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CSS/style.css'
+
+function Jobs() {
+  return (
+    <div>
+        <div className='title'>Github <span>Jobs</span></div>
+    </div>
+  )
+}
+
+export default Jobs
