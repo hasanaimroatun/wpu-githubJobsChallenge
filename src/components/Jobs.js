2ef55e943cc02a956cc8b7d918ad955288774699
hasanaimroatun <imroatunhasana@gmail.com>
2022-12-09 14:59:40 +0700

    import icon and add child elements in heroImg class element

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './CSS/style.css'
+import { MdOutlineWorkOutline } from 'react-icons/md'
 
 function Jobs() {
   return (
     <div>
         <div className='title'>Github <span>Jobs</span></div>
-        <div className='heroImg'></div>
+        <div className='heroImg'>
+          <div className="input-group mb-3 position-absolute bottom-0 start-50 translate-middle">
+            <MdOutlineWorkOutline size='1.2em' color='#B9BDCF' className='iIcon'/>
+            <input type="text" className="form-control me-1" placeholder="Title, companies, expertise or benefits" aria-label="Title, companies, expertise or benefits" aria-describedby="button-addon2"/>
+            <button className="btn btn-primary" type="button" id="button-addon2">Search</button>
+          </div>
+        </div>
     </div>
   )
 }
