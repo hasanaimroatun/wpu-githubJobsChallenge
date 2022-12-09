e9b5dfd14b8a7435176fb0408501099313be9991
hasanaimroatun <imroatunhasana@gmail.com>
2022-12-09 10:24:54 +0700

    import Jobs and delete imported css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Jobs from './components/Jobs'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Jobs />
     </div>
   );
 }
